bb26a233a7654a16cffc1ed99757ff9c6e4ba844
Aaron <leebaaron@outlook.com>
2026-03-31 15:06:17 -0500

    First attempt at submissions 1 - 4

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,3 @@
 # Create a wallet with the name "builderswallet".
+
+bitcoin-cli -regtest -named createwallet wallet_name="builderswallet" descriptors=true
--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,7 @@
 # Create a native segwit address and get the public key from the address.
+
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest -named getnewaddress address_type="bech32")
+
+PUBKEY=$(bitcoin-cli -regtest -named getaddressinfo address=$SEGWIT_ADDRESS | jq -r '.pubkey')
+
+echo $PUBKEY
