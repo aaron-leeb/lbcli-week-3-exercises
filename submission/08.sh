d1a62aa17810081d3daa08a87290cf4c21a587ec
Aaron <leebaaron@outlook.com>
2026-03-31 23:58:30 -0500

    Updated jq process for 08

--- submission/08.sh
+++ submission/08.sh
@@ -3,6 +3,6 @@
 
 psbt="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
 
-receiver_address=$(bitcoin-cli -regtest -named decodepsbt psbt=$psbt | jq -r '.tx.vout[0].scriptPubKey.addresses[0]')
+receiver_address=$(bitcoin-cli -regtest -named decodepsbt psbt=$psbt | jq -r '.tx.vout[0].scriptPubKey.address')
 
 echo $receiver_address
