0de3432e5ce5fb59fcbe457c9d379175197d90a0
Aaron <leebaaron@outlook.com>
2026-03-31 23:50:36 -0500

    Reattempt 07

--- submission/07.sh
+++ submission/07.sh
@@ -3,6 +3,6 @@
 
 psbt="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
 
-output_value=$(bitcoin-cli -regtest -named decodepsbt psbt=$psbt | jq -r '.tx' | jq -r '.vout[]' | jq 'map(.value)' | jq 'add * 100000000')
+output_value=$(bitcoin-cli -regtest -named decodepsbt psbt=$psbt | jq '[.tx.vout[].value] | add * 100000000')
 
 echo $output_value
